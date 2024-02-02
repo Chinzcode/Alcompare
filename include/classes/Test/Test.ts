d66b3e7f257d96d101d631446afa15a408d8ae30
Daniel Ravndal <danielcravndal@gmail.com>
2024-02-02 12:03:59 +0100

    chore: add example files to folder structure

---
+++ include/classes/Test/Test.ts
@@ -0,0 +1,11 @@
+class Test {
+    greeting: string;
+
+    constructor(message: string) {
+        this.greeting = message;
+    }
+
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
