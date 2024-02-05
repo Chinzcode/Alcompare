11192e4bf692b3b73ad48f555cba77e9332bdd68
Daniel Ravndal <danielcravndal@gmail.com>
2024-02-05 10:03:42 +0100

    chore: cleanup code

--- config/setup.php
+++ config/setup.php
@@ -1,8 +1,3 @@
 <?php
-
 declare(strict_types=1);
 require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
-use Alcompare\util\Twig\TwigEngine;
-
-$twig = TwigEngine::getInstance();
-echo $twig->render("/templates/frontpage.html.twig");
