daccf310e09313a6ff477249ee7283593baae071
Trym <chinzcoding@gmail.com>
2024-02-20 10:24:57 +0100

    chore: Add UserDatabaseQueries for user-related database queries

---
+++ include/util/PHP/User/UserDatabaseQueries.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Alcompare\util\PHP\User;
+
+use PDO;
+
+class UserDatabaseQueries
+{
+    public function getUsername(object $pdo, string $username)
+    {
+        $query = "SELECT username FROM users WHERE username = :username;";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":username", $username);
+        $stmt->execute();
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function getEmail(object $pdo, string $email)
+    {
+        $query = "SELECT username FROM users WHERE email = :email;";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }
+}
