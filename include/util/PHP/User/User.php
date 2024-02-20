77366d94152b1f28da8bfe866d8ca7836224358b
Trym <chinzcoding@gmail.com>
2024-02-20 10:23:24 +0100

    chore: Add user class for representing user data

---
+++ include/util/PHP/User/User.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Alcompare\util\PHP\User;
+
+class User
+{
+    protected string $username;
+    protected string $pwd;
+    protected string $email;
+
+    /**
+     * Get the value of name
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of pwd
+     */
+    public function getPwd()
+    {
+        return $this->pwd;
+    }
+
+    /**
+     * Set the value of pwd
+     *
+     * @return  self
+     */
+    public function setPwd($pwd)
+    {
+        $this->pwd = $pwd;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
