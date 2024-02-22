96e2bf8c765a8f0bced691a0df9816a7657323f0
Trym <chinzcoding@gmail.com>
2024-02-22 10:46:45 +0100

    chore: Update Base class, added globalVariables to twig template

--- include/classes/Base/Base.php
+++ include/classes/Base/Base.php
@@ -1,15 +1,25 @@
 <?php
+
 namespace Alcompare\classes\Base;
+
 require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 use Alcompare\util\Twig\TwigEngine;
+use Alcompare\util\PHP\SessionManager\SessionManager;
 
-class Base {
+class Base
+{
     private $twig;
+    protected $sessionManager;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->twig = TwigEngine::getInstance();
+        $this->sessionManager = new SessionManager();
+        $this->twig->addGlobalVariable("isUserLoggedIn", $_SESSION["userId"]);
+        $this->twig->addGlobalVariable("username", $_SESSION["username"]);
     }
-    public function render(string $name, array $context = array()) {
+    public function render(string $name, array $context = array())
+    {
         return $this->twig->render($name, $context);
     }
 }
