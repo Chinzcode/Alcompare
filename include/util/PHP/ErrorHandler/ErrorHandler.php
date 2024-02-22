d156371c09238d1957d6c4f775b6825fa56a6a39
Trym <chinzcoding@gmail.com>
2024-02-22 10:57:11 +0100

    chore: Update ErrorHandler class
    Add isLoginInputEmpty, isUserCredentialsInvalid methods

--- include/util/PHP/ErrorHandler/ErrorHandler.php
+++ include/util/PHP/ErrorHandler/ErrorHandler.php
@@ -3,34 +3,74 @@
 namespace Alcompare\util\PHP\ErrorHandler;
 
 use Alcompare\util\PHP\User\UserDatabaseQueries;
+use PDO;
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/config/setup.php';
 
 class ErrorHandler
 {
     protected UserDatabaseQueries $userDbQuery;
 
+    /**
+     * ErrorHandler constructor.
+     */
     public function __construct()
     {
         $this->userDbQuery = new UserDatabaseQueries();
     }
+
+    /**
+     * Checks if any of the input fields are empty.
+     */
     public function isInputEmpty(string $username, string $pwd, string $email): bool
     {
         return empty($username) || empty($pwd) || empty($email);
     }
 
+    /**
+     * Checks if the email is invalid.
+     */
     public function isEmailInvalid(string $email): bool
     {
         return !filter_var($email, FILTER_VALIDATE_EMAIL);
     }
 
-    public function isUsernameTaken(object $pdo, string $username): bool
+    /**
+     * Checks if the username is already taken.
+     */
+    public function isUsernameTaken(PDO $pdo, string $username): bool
     {
         $result = $this->userDbQuery->getUsername($pdo, $username);
         return !empty($result);
     }
 
-    public function isEmailRegistered(object $pdo, string $email): bool
+    /**
+     * Checks if the email is already registered.
+     */
+    public function isEmailRegistered(PDO $pdo, string $email): bool
     {
         $result = $this->userDbQuery->getEmail($pdo, $email);
         return !empty($result);
+    }
+
+    /**
+     * Checks if the login input fields are empty.
+     */
+    public function isLoginInputEmpty(string $username, string $pwd): bool
+    {
+        return empty($username) || empty($pwd);
+    }
+
+    /**
+     * Checks if the username or password is invalid.
+     */
+    public function isUserCredentialsInvalid(PDO $pdo, string $username, string $pwd): bool
+    {
+        $user = $this->userDbQuery->getUser($pdo, $username);
+        if (!$user) {
+            return true;
+        }
+        $hashedPwd = $user["pwd"];
+        return !password_verify($pwd, $hashedPwd);
     }
 }
